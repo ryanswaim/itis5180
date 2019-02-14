68de83cfc1d03828db811e68958bc6bed72cf7d3
rstoller0 <rockfordstoller@gmail.com>
2019-02-14 17:30:03 -0500

    re-push

--- Homework02/app/src/main/java/com/example/homework02/MainActivity.java
+++ Homework02/app/src/main/java/com/example/homework02/MainActivity.java
@@ -3,18 +3,11 @@ package com.example.homework02;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//Homework 02
-//Group 9
-//Rockford Stoller
-//Ryan Swaim
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setTitle(R.string.title_text);
     }
 }
